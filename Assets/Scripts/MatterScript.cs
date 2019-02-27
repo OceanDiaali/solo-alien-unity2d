6d995adbf787386b7a34e443215cb74d60cf832c
OceanDiaali <ocean.diaali@gmail.com>
2019-02-27 19:51:01 +0100

    add logic for 'grey matter','player score', and 'laser fire' controls

---
+++ Assets/Scripts/MatterScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatterScript : MonoBehaviour {
+
+	public int speed = -3;
+	Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		if(col.gameObject.name == "laser(Clone)")
+		{
+			Destroy (col.gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		rb2d.velocity = new Vector2(0.0f, speed);
+	}
+
+	void OnBecameInvisible() {
+		Destroy (gameObject);
+	}
+
+}
---
+++ Assets/Scripts/SoundManagerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour {
+
+	public static AudioClip laserFireSound, matterHitSound;
+	static AudioSource audioSrc;
+
+	// Use this for initialization
+	void Start () {
+		laserFireSound = Resources.Load<AudioClip> ("lazerzap");
+		matterHitSound = Resources.Load<AudioClip> ("matterHit");
+
+		audioSrc = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static void PlaySound(string clip) {
+		switch (clip) {
+		case "lazerzap":
+			audioSrc.PlayOneShot (laserFireSound);
+			break;
+		case "matterHit":
+			audioSrc.PlayOneShot (matterHitSound);
+			break;
+		}
+	}
+}
